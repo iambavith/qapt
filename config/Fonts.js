c0cc0329942371b118c5c62a147af6399fb7d335
Bavith <55197783+iambavith@users.noreply.github.com>
2021-02-15 17:14:13 +0530

    Add files via upload

---
+++ config/Fonts.js
@@ -0,0 +1,5 @@
+export default {
+  Anaktoria: require('../assets/fonts/Anaktoria.otf'),
+  MerriweatherRegular: require('../assets/fonts/MerriweatherRegular.ttf'),
+  NunitoExtraLight: require('../assets/fonts/NunitoExtraLight.ttf'),
+};
